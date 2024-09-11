54f9d68d97036882a17913f3e27db6046a6a9c67
HP <awadhrajpatel95@gmail.com>
2024-09-11 17:17:04 +0530

    Added Successfully Logout

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './Pages/Home'
+import Signup from './Pages/Auth/Signup'
+import Login from './Pages/Auth/Login'
+import NotFound from './Pages/NotFound'
+import Denied from './Pages/Denied'
+import AddProduct from './Pages/Admin/Addproduct'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Routes>
+      <Route path='/'element={<Home/>}/>
+      <Route path="/denied" element={<Denied />} />
+      <Route path='/auth/signup' element={<Signup />} />
+      <Route path='/auth/login' element={<Login />} />
+
+      <Route path='/admin/addProduct' element={<AddProduct />} />
+
+      <Route path='*' element={<NotFound />} />
+    </Routes>
     </>
   )
 }
---
+++ src/Layouts/Layout.jsx
@@ -0,0 +1,80 @@
+import { useDispatch, useSelector } from 'react-redux';
+import Pizzalogo from '../assets/Images/pizza1.png';
+import Footer from '../Components/Footer';
+import { Link } from 'react-router-dom';
+import { logout } from '../Redux/Slices/AuthSlice';
+
+function Layout({ children }) {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const dispatch = useDispatch();
+
+  async function handleLogout(e) {
+    e.preventDefault();
+    dispatch(logout());
+    
+}
+
+  return (
+    <div> 
+      <nav className="flex items-center justify-around h-16 text-[#6B7280] font-mono border-none shadow-md">
+        <div
+          className="flex items-center justify-center"
+          onClick={() => navigate("/")}
+        >
+          <p>Pizza App</p>
+          <img src={Pizzalogo} alt="Pizza logo" />
+        </div>
+
+        <div className="hidden md:block">
+          <ul className="flex gap-4">
+            <li className="hover:text-[#FF9110]">
+              {" "}
+              <p>Menu </p>
+            </li>
+
+            <li className="hover:text-[#FF9110]">
+              {" "}
+              <p>Services </p>
+            </li>
+
+            <li className="hover:text-[#FF9110]">
+              {" "}
+              <p>About </p>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+         <ul className='flex gap-4'>
+           <li className='hover:text-[#FF9110]'>
+              {isLoggedIn ? (
+              <Link onClick={handleLogout}>Logout</Link>
+              ) : (
+                <Link to={'/auth/login'}>Login</Link>
+              )}
+          </li>
+
+          {/* {isLoggedIn && ( */}
+          {/* <Link to={'/cart'}> */}
+           {/* <li>
+             <img src={CartIcon} className='w-8 h-8 inline' />
+             {' '}
+             <p className='text-black inline'>{cartsData?.items?.length}</p>
+          </li>
+          </Link>            
+          )}; */}
+          
+            </ul>
+          </div>
+
+      </nav>
+
+      {children}
+      
+      <Footer/>
+
+    </div>
+  );
+}
+
+export default Layout;
